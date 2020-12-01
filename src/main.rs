8338f8503bb80c3810dd4dfbca4bf34cbe6b5820
Richard Rast <richard.rast@gmail.com>
2020-12-01 08:49:58 -0500

    chore: init

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use std::env;
+
+mod day1;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        println!("Usage: [run] [problemNumber]");
+        println!("Example: cargo run --release -- 1a");
+    }
+
+    let start = std::time::Instant::now();
+
+    let out: String = match args.get(1).unwrap().as_str() {
+        "1a" => day1::run_1a().to_string(),
+
+        other => {
+            format!("Unrecognized problem number {}", other)
+        }
+    };
+
+    println!("{}", out);
+    println!("Process took {:.3} seconds", start.elapsed().as_secs_f32());
+}
